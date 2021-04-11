5e7149b62b1653905577166cc7030ef770df49aa
Max Max <maximzh@post.bgu.ac.il>
2021-04-11 23:10:20 +0300

    Code checked on big corpora

--- NLP_HW1/317649606.py
+++ NLP_HW1/317649606.py
@@ -1,5 +1,6 @@
 import math
 import random
+import datetime
 
 
 class Spell_Checker:
@@ -21,16 +22,16 @@ class Spell_Checker:
         pass
 
     def evaluate(self, text):
-        pass
+        return self.lm.evaluate(text)
 
     class Language_Model:
 
         def __init__(self, n=3, chars = False):
             self.n = n
             self.chars = chars
-            self.model_dict = None
+            self.model_dict = None # key: N gram, value: how much appeared
             self.model_trimmed_dict = {} # key: N-1 gram, value: how much appeared
-            self.model_context_dict = {} #key N-1 gram, value: list of N grams that N-1 gram is their prefix
+            self.model_context_dict = {} # key N-1 gram, value: list of N grams that N-1 gram is their prefix
 
         def build_model(self, text):
             if self.model_dict is None: self.model_dict = {}
@@ -66,19 +67,24 @@ class Spell_Checker:
             return self.n
 
         def generate(self, context=None, n=20):
-            #What to do if the context is not in the dictionary? !!!
+            # What to do if the context is not in the dictionary? !!!
             if context is not None and len(context.split()) >= n:
                 return (context.split())[0:n]
             if context is None:
-                context_keys = list(self.model_context_dict.keys())
-                random_number = random.randint(0,len(context_keys) - 1) #Check for right sampling !!!
-                context = context_keys[random_number]
+                # if sampling is by uniform
+                # context = self.choose_sample('uniform')
+
+                # if sampling is by choise
+                context = self.choose_sample('choise') # Check for right sampling !!!
+
             generated_text = context.split()
             while len(generated_text) < n:
                 last_context = generated_text[-(self.n-1):]
-                last_context_list = self.model_context_dict[' '.join(last_context)]
-                random_number = random.randint(0, len(last_context_list)-1) #Check for right sampling !!!
-                generated_text.append(last_context_list[random_number])
+                try:
+                    last_context_list = self.model_context_dict[' '.join(last_context)]
+                    random_number = random.randint(0, len(last_context_list)-1) # Check for right sampling !!!
+                    generated_text.append(last_context_list[random_number])
+                except KeyError: break # Stops when context in not in the text
             return ' '.join(generated_text)
 
         def evaluate(self, text):
@@ -103,22 +109,34 @@ class Spell_Checker:
             lower_c = 0 if trimmed_ngram not in self.model_trimmed_dict else self.model_trimmed_dict[trimmed_ngram]
             return (upper_c+1) / (lower_c+V)
 
-        def normalize_text(self, text):
-            lowered_text = text.lower()
-            set_punctuations = {char for char in '''!()-[]{};:'"\,<>./?@#$%^&*_~'''}
-            normalized_text = ''
-            for index in range(len(lowered_text)):
-                if lowered_text[index] not in set_punctuations:
-                    normalized_text += lowered_text[index]
-                else:
-                    try:
-                        if lowered_text[index+1] != ' ' and lowered_text[index-1] != ' ':
-                            normalized_text += ' '
-                    except IndexError:
-                        continue
-            return normalized_text
+        def choose_sample(self, how):
+            if how == 'uniform':
+                context_keys = list(self.model_context_dict.keys())
+                random_number = random.randint(0,len(context_keys) - 1)
+                return context_keys[random_number]
+            elif how == 'choise':
+                context_keys, weights = [], []
+                for key, value in self.model_context_dict.items():
+                    context_keys.append(key)
+                    weights.append(len(value))
+                return (random.choices(context_keys, weights, k=1))[0]
 
 
+
+def normalize_text(text):
+    lowered_text = text.lower()
+    set_punctuations = {char for char in '''!()-[]{};:'"\,<>./?@#$%^&*_~'''}
+    normalized_text = ''
+    for index in range(len(lowered_text)):
+        if lowered_text[index] not in set_punctuations:
+            normalized_text += lowered_text[index]
+        else:
+            try:
+                if lowered_text[index+1] != ' ' and lowered_text[index-1] != ' ':
+                    normalized_text += ' '
+            except IndexError:
+                continue
+    return normalized_text
 
 
 def who_am_i():
@@ -128,8 +146,34 @@ def who_am_i():
 #---------------------------- Tests ----------------------------#
 s_c = Spell_Checker()
 l_m = s_c.Language_Model(n = 3)
-l_m.build_model('Maxim is student and is student good in the university')
-print(l_m.model_dict)
-print(l_m.model_trimmed_dict)
-print(l_m.model_context_dict)
-print(l_m.generate('is student',n = 4))
+s_c.add_language_model(l_m)
+#--- my tests ---#
+# l_m.build_model('Maxim is student and is student')
+# print(l_m.model_dict)
+# print(l_m.model_trimmed_dict)
+# print(l_m.model_context_dict)
+# print(l_m.generate('is student',n = 4))
+
+#--- big.txt tests ---#
+# big = open('big.txt', 'r').read()
+# start = datetime.datetime.now()
+# big_normlized = normalize_text(big)
+# end = datetime.datetime.now()
+# print(f'Normlization took:   {end - start}')
+# start = datetime.datetime.now()
+# l_m.build_model(big_normlized)
+# end = datetime.datetime.now()
+# print(f'Building the model took:   {end - start}')
+
+#--- corpus.data tests ---#
+corpus = open('corpus.data', 'r').read()
+corpus = ' '.join(corpus.split('<s>'))
+start = datetime.datetime.now()
+corpus_normlized = normalize_text(corpus)
+end = datetime.datetime.now()
+print(f'Normlization took:   {end - start}')
+start = datetime.datetime.now()
+l_m.build_model(corpus_normlized)
+end = datetime.datetime.now()
+print(f'Building the model took:   {end - start}')
+
